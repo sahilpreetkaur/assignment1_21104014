2f0c62d55c1a930becd612e0edffe2ffe5141fee
sahilpreetkaur <97119595+sahilpreetkaur@users.noreply.github.com>
2022-02-27 20:04:08 +0530

    Add files via upload

---
+++ assign4_txt.py
@@ -0,0 +1,50 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:/Users/sidsam/Downloads/assign4.py ================
+move disk 1 from tower 1 to tower 2
+move disk 2 from tower 1 to tower3
+move disk 1 from tower 2 to tower3
+move disk 3 from tower 1 to tower 2
+move disk 1 from tower3 to tower 1
+move disk 2 from tower3 to tower 2
+move disk 1 from tower 1 to tower 2
+enter no. of rows : 5
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+enter first no. 6
+enter second no. 2
+display true if the function is callable:  False
+display true if all values are non zero :  False
+unfiltered tuple :  (3, 0, 4, 5, 6)
+filtered tuple:  (3, 0, 4)
+converted into set data type :  {0, 3, 4}
+immutable set frozenset({0, 3, 4})
+maximum value in the set :  4
+Hash value of the maximum value :  4
+Object created
+NAME     ROLL_NUMBER
+sahilpreet 21104014
+Object deleted
+
+NAME  SALARY
+Mehak 40000
+Ashok 50000
+Viren 60000
+
+updating details of mehak... 
+updated details of mehak :
+NAME  SALARY
+Mehak 70000
+Enter the first word: tree
+
+Enter a new meaningful word to test your friendship: reet
+friendship is fake
